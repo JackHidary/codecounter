3ea15131aa32f40a4998aa3affa61bbae75c21fa
Jackhidary <Jackhidary@gmail.com>
2017-02-26 11:58:55 -0800

    new line

--- linecounter.py
+++ linecounter.py
@@ -1,15 +1,21 @@
-
-
 from google.cloud import bigquery as bq
 
-
+ID_INDEX = 0
+SIZE_INDEX = 1
+CONTENT_INDEX = 2
+BINARY_INDEX = 3
 
 def linecount():
-
   client = bq.Client()
-  query="SELECT * FROM [bigquery-public-data:github_repos.sample_contents] LIMIT 100"
+  query="SELECT * FROM [bigquery-public-data:github_repos.sample_contents] WHERE binary = false"
   query_results = client.run_sync_query(query)
+  query_results.run(allowLargeResults=True)
+  rows, total_rows, page_token = query_results.fetch_data()
 
+
+  with open("githubFiles.txt", 'w') as outFile:
+      for githubFile in rows:
+          outFile.write(githubFile[CONTENT_INDEX])
 
 
 
